eff8ac35dba16eb7cbc3b1d5ac2e5b8da2403cd0
laportenier <lilly.portenier@gmail.com>
2019-12-10 18:22:34 -0500

    finally got data from user to push to api/friends and calculated difffrence

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -1,11 +1,32 @@
 var userData = require("../data/friends");
 
-module.exports = function(app) {
+module.exports = function (app) {
     app.get("/api/friends", function (req, res) {
         res.json(userData);
     });
+    // app.get("/survey", function(req, res){
+    //     res.json(newUser);
+    // })
 
-    app.post("/api/friends", function(req, res) {
-        userData.push(req.body)
+    app.post("/api/friends", function (req, res) {
+        var newUserScores = req.body.scores
+        var newUser = req.body;
+        var friendDiffArray = [];
+        // console.log(newUser);
+
+        // console.log(userData);
+        for (i = 0; i < userData.length; i++) {
+            console.log(newUser[i],"NU");
+
+            console.log(newUserScores[i]);
+            var diff = 0;
+
+            for (k = 0; k < newUserScores.length; k++) {
+                diff += (Math.abs(parseInt(userData[i].scores[k]) - parseInt(newUserScores[k])));
+            }
+            friendDiffArray.push(diff);
+        }
+        console.log(friendDiffArray, "dif afay");
+        
     })
 }
